1105afe74190d3a78fda2bd7b2240887e6cadd90
jan-zelaznog <jan.zelaznog@gmail.com>
2025-04-26 10:25:34 -0600

    Initial Commit

---
+++ Mascotas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mascotas
+//
+//  Created by Ángel González on 26/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
